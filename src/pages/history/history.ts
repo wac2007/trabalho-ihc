6870aa94dad2bf2ac9a641386786ccbdefa515fa
Wallace Coelho <wac.almeida@gmail.com>
2017-11-27 23:27:18 -0200

    Refactor history page

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ViewInfoPage } from '../view-info/view-info';
+
+
 @Component({
   selector: 'page-history',
   templateUrl: 'history.html'
 })
 export class HistoryPage {
-
+  private year = 2017;
+  private month = 11;
   constructor(public navCtrl: NavController) {
+  }
+
+  private viewInfo() {
+    this.navCtrl.push(ViewInfoPage);
   }
 }
